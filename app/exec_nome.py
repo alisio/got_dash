e95d687f1f5edfb5354790f2e86d3b23146cef74
Antonio Alisio de Meneses Cordeiro <alisio.meneses@gmail.com>
2024-05-03 11:09:50 -0300

    add side panel text

--- app/exec_nome.py
+++ app/exec_nome.py
@@ -11,13 +11,22 @@ import os
 deaths_df = pd.read_csv('./data/deaths.csv')
 
 # constantes
-
-
 img_path = "./img"
 principais_personagens = [
     'Arya Stark', 'Cersei Lannister', 'Jon Snow', 'Olenna Tyrell', 'Sansa Stark', 'Tyrion Lannister', 'Bran Stark', 'Daenerys Targaryen', 'Jorah Mormont', 'Robb Stark', 'The High Sparrow', 'Catelyn Stark', 'Jaime Lannister', 'Littlefinger', 'Samwell Tarly', 'The Hound'
 ]
 principais_personagens.sort()
+
+texto_sidebar = """
+Este dashboard está focado no dataset de mortes sobre a série de TV Game of Thrones. O enredo fictício dessa série trata da dispuda de poder entre várias famílias e outros agrupamentos ou alianças que nesse caderno vamos chamar de facções. Devido à natureza dessa disputa, várias baixas vão ocorrer ao longo dos episódios. As perguntas que podem surgir nesse sentido envolvem: 
+
+* Qual o personagem que mais causou mortes? 
+* Qual foi o método mais utilizado para este fim? 
+* Qual casa/facção sofreu mais baixas? 
+
+Vamos fazer uma análise com o auxílio da biblioteca streamlit.
+"""
+
 
 # Variáveis
 lista_personagens = list(deaths_df['name'].unique())
@@ -27,7 +36,9 @@ top_executores.columns = ["Executor","Contagem"]
 top_metodos = deaths_df['method'].value_counts().reset_index()
 top_metodos.columns = ["Metodo","Contagem"]
 
+# Página
 
+st.sidebar.write(texto_sidebar)
 
 pers_col1, pers_col2, pers_col3, pers_col4 = st.columns(4)
 
@@ -69,27 +80,43 @@ with pers_col4:
 
     st.altair_chart(chart_metodos, use_container_width=True)
 
-col_body1,col_body2 = st.columns(2)
+# col_body1,col_body2 = st.columns(2)
 
-with col_body1:
-    st.image('./img/westeros.jpeg',use_column_width="auto")
+# with col_body1:
+    # st.image('./img/westeros.jpeg',use_column_width="auto")
 
-with col_body2:
-    st.markdown("### Total De Execuções do Personagem - Cumulativo")
-    # Criar df cumulativo
-    deaths_df_personagem = deaths_df[deaths_df["killer"] == personagem]
-    num_execucoes_por_season_ep = deaths_df_personagem.groupby(['season', 'episode']).size().reset_index()
-    num_execucoes_por_season_ep.columns = ['Temporada', 'Episódio', 'Execuções']
-    num_execucoes_por_season_ep['Cumulativo'] = num_execucoes_por_season_ep['Execuções'].cumsum()
-    num_execucoes_por_season_ep['Ep'] = 'S' + num_execucoes_por_season_ep['Temporada'].astype(str) + 'E' + num_execucoes_por_season_ep['Episódio'].astype(str)
+# with col_body2:
+st.markdown("### Total De Execuções do Personagem - Cumulativo")
+# Criar df cumulativo
+deaths_df_personagem = deaths_df[deaths_df["killer"] == personagem]
+num_execucoes_por_season_ep = deaths_df_personagem.groupby(['season', 'episode']).size().reset_index()
+num_execucoes_por_season_ep.columns = ['Temporada', 'Episódio', 'Execuções']
+num_execucoes_por_season_ep['Temporada'] = num_execucoes_por_season_ep['Temporada'].astype(str).apply(lambda x: x.zfill(2))
+num_execucoes_por_season_ep['Episódio'] = num_execucoes_por_season_ep['Episódio'].astype(str).apply(lambda x: x.zfill(2))
+num_execucoes_por_season_ep['Cumulativo'] = num_execucoes_por_season_ep['Execuções'].cumsum()
+num_execucoes_por_season_ep['Ep'] = 'S' + num_execucoes_por_season_ep['Temporada'] + 'E' + num_execucoes_por_season_ep['Episódio']
 
-    st.line_chart(num_execucoes_por_season_ep,x='Ep',y='Cumulativo',color = '#FF0000')
+# Criar um DF contendo todas as temporadas e episódios em formato S0XE0Y
+df_ep = deaths_df[['season','episode']].copy()
+df_ep['season'] = df_ep['season'].astype(str).apply(lambda x: x.zfill(2))
+df_ep['episode'] = df_ep['episode'].astype(str).apply(lambda x: x.zfill(2))
+df_ep['Ep'] = 'S' + df_ep['season'] + 'E' + df_ep['episode']
+df_ep = df_ep.drop_duplicates()
+
+df_exec_cum = pd.merge(df_ep['Ep'],num_execucoes_por_season_ep,'outer','Ep')
+df_exec_cum['Execuções'] = df_exec_cum['Execuções'].ffill()
+df_exec_cum['Cumulativo'] = df_exec_cum['Cumulativo'].ffill()
+
+st.line_chart(df_exec_cum,x='Ep',y='Cumulativo',color = '#FF0000')
 
 st.divider()
 
+
+st.columns(3)[1].markdown("## Top 10 Geral")
+
 col_bottom1,col_bottom2 = st.columns(2)
 with col_bottom1:
-    st.markdown("## Top Executores")
+    st.markdown("### Executores")
 
     chart_executores = (
     alt.Chart(top_executores[:10]).mark_bar().encode(
@@ -104,7 +131,7 @@ with col_bottom1:
 
 
 with col_bottom2:
-    st.markdown("## Top Métodos")
+    st.markdown("### Top Métodos")
     chart_metodos = (
         alt.Chart(top_metodos[:10]).mark_bar().encode(
                 x='Contagem:Q',
